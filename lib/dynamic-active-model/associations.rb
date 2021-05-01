1b06441a156a899e49fb291db9fb1fea268e7541
Doug Youch <dougyouch@gmail.com>
2021-05-01 08:34:25 -0400

    Revert "updated assiociations to support multiple databases"
    This reverts commit 9bbbb06f07c76dd5dd5c06c0c0b7298570799f25.

--- lib/dynamic-active-model/associations.rb
+++ lib/dynamic-active-model/associations.rb
@@ -2,13 +2,13 @@
 
 module DynamicActiveModel
   # DynamicActiveModel::Associations iterates over the models of a
-  #  databases and adds has_many and belongs_to based on foreign keys
+  #  database and adds has_many and belongs_to based on foreign keys
   class Associations
-    attr_reader :databases
+    attr_reader :database
 
-    def initialize(databases)
-      @databases = Array(databases)
-      @foreign_keys = models.flatten.each_with_object({}) do |model, hsh|
+    def initialize(database)
+      @database = database
+      @foreign_keys = database.models.each_with_object({}) do |model, hsh|
         hsh[model.table_name] = ForeignKey.new(model)
       end
     end
@@ -21,7 +21,7 @@ module DynamicActiveModel
     def build!
       foreign_key_to_models = create_foreign_key_to_model_map
 
-      models.each do |model|
+      @database.models.each do |model|
         model.column_names.each do |column_name|
           next unless foreign_key_to_models[column_name.downcase]
 
@@ -35,10 +35,6 @@ module DynamicActiveModel
     end
 
     private
-
-    def models
-      databases.map(&:models).flatten
-    end
 
     def add_relationships(relationship_name, model, belongs_to_model, foreign_key)
       add_belongs_to(relationship_name, model, belongs_to_model, foreign_key)
